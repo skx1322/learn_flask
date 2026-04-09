e1985ecfd0b8e23df944e80c5dd3da99e2282a56
skx1322 <kaixing636@gmail.com>
2026-04-09 22:55:26 +0800

    Learning JSON, Query and Param

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, jsonify, send_file     
+from data import book_data, serial
+
+from flask import Flask, request, send_file     
 app = Flask(__name__) 
 
-@app.route('/', methods=['GET'])       
-def hello(): 
-    filePath = '/public/FuHua27.png'
+@app.route('/<filename>', methods=['GET'])       
+def hello(filename): 
+    filePath = f'../public/{filename}'
     if not filePath:
         print("Path not found.")
         return {"message": "Not found"}, 404
@@ -12,7 +14,17 @@ def hello():
 
 @app.route("/book", methods=['GET'])
 def log():
-    return {"message": "Success", "data": "hello world"}, 200
+    query = request.args.get('name')
+    right_book = [obj for obj in serial if obj['id'] == query]
+
+    if not right_book:
+        return {"message": "Not found"}, 404
+
+    return {
+        "message": "Success", 
+        "data": f"Query: {query}",
+        "book": right_book
+    }, 200
 
 @app.route("/book", methods=['POST'])
 def book():
---
+++ src/data.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+json_path = os.path.join(BASE_DIR, 'data.json')
+
+try:
+    with open(json_path, 'r') as file:
+        book_data = json.load(file)
+    
+except FileNotFoundError:
+    print("Error: The file 'data.json' was not found.")
+
+
+serial = book_data['book']
+for x in serial:
+    print(x['id'])
+
+# print(serial)
+# print(book_data = json.dumps(data, indent=4))
